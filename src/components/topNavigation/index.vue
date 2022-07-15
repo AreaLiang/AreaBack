fe914858d179eea17a5118eee7fb51d1d298502b
AreaLiang <137739760@qq.com>
2022-07-15 09:00:30 +0800

    7-15

--- src/components/topNavigation/index.vue
+++ src/components/topNavigation/index.vue
@@ -2,34 +2,38 @@
 	<div class="top-navigation">
 		<div class="left"></div>
 		<div class="right">
-			46
+			<div>
+				<el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size='60'></el-avatar>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	export default{
-		name:'topNavigation',//顶部导航栏
-		data(){
-			return{
-				
+	export default {
+		name: 'topNavigation', //顶部导航栏
+		data() {
+			return {
+
 			}
 		},
-		methods:{
-		
+		methods: {
+
 		},
-		components:{
-			
+		components: {
+
 		}
 	}
 </script>
 
 <style scoped lang="less">
-	.top-navigation{
+	.top-navigation {
 		box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
 		width: 100%;
 		display: flex;
 		justify-content: space-between;
-		height: 100px;
+		.right{
+			margin: 5px 10px;
+		}
 	}
 </style>
